393c7db2c6f0188bda8fd105c5864a402b827c33
Alejandro Perdomo González <apergot95@gmail.com>
2019-10-04 12:14:27 +0000

    dynamic news films per month

--- src/app/services/movies-service.ts
+++ src/app/services/movies-service.ts
@@ -17,11 +17,26 @@ export class MoviesService {
   private executeQuery<T>(query: string){
     query = URL+query;
     query += `&api_key=${apiKey}`;
-    return this.http.get(query);
+    return this.http.get<T>(query);
   }
 
   getTheatreMovies(){
-    return this.executeQuery<MovieDBResponse>('/discover/movie?primary_release_date.gte=2019-09-01&primary_release_date.lte=2019-09-28');
+
+    const today = new Date();
+    const lastMonthDay = new Date(today.getFullYear(), today.getMonth()+1, 0).getDate();
+    const month = today.getMonth() + 1;
+    let monthString;
+    
+    if(month<10){
+      monthString = '0'+ month;
+    }else{
+      monthString = month;
+    }
+
+    const start = `${today.getFullYear()}-${monthString}-01`;
+    const end   = `${today.getFullYear()}-${monthString}-${lastMonthDay}`;
+    
+    return this.executeQuery<MovieDBResponse>(`/discover/movie?primary_release_date.gte=${start}&primary_release_date.lte=${end}`);
   }
 
 }
